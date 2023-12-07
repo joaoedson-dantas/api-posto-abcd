cb1ac303aef4000e1ee7b00818d3b0bf915cb09d
joaoedson <joaoedson.dantas@outlook.com>
2023-12-07 18:58:01 -0300

    feat: creating service ExceptionHandlerController

---
+++ src/main/java/posto/abcd/api/infra/exceptions/ExceptionHandlerController.java
@@ -0,0 +1,38 @@
+package posto.abcd.api.infra.exceptions;
+
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleException404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleException400(MethodArgumentNotValidException exception) {
+
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(dataValidationErrors::new).toList());
+    }
+
+
+
+    private record dataValidationErrors(String field, String message) {
+        public dataValidationErrors(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    } // dto - para devolver o erro
+}
+
+
+
+// Classe responsável por isolar o tratamento de erros na api; @RestControllerAdvice
+// em cima do metodo informar para qual ecessao o metoo handleExcepetion404 vai ser chama?
