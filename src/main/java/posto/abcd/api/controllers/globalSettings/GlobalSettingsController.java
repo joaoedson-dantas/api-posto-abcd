ffc0b705480efde219660fbff5380f35151966d6
joaoedson <joaoedson.dantas@outlook.com>
2023-12-08 22:27:37 -0300

    refactor: adding route getting all global settings

--- src/main/java/posto/abcd/api/controllers/globalSettings/GlobalSettingsController.java
+++ src/main/java/posto/abcd/api/controllers/globalSettings/GlobalSettingsController.java
@@ -2,11 +2,11 @@ package posto.abcd.api.controllers.globalSettings;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import posto.abcd.api.dtos.globalSettings.GlobalSettingsDataRequest;
 import posto.abcd.api.dtos.globalSettings.GlobalSettingsDataResponse;
@@ -26,6 +26,12 @@ public class GlobalSettingsController {
 
         var uri = uriBuilder.path("/global-settings/{id}").buildAndExpand(globalSettingsEntity.getId()).toUri();
         return ResponseEntity.created(uri).body(new GlobalSettingsDataResponse(globalSettingsEntity));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<GlobalSettingsDataResponse>> getAllGlobalSettings(@PageableDefault(size = 10, page = 0, sort = {"id"}) Pageable pagination) {
+        var globalSettingsList = globalSettingsService.GetAllGlobalSettings(pagination);
+        return ResponseEntity.ok(globalSettingsList);
     }
 
 }
