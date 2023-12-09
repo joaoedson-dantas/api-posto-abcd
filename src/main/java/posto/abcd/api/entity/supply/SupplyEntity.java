06928a6de753890f6d1c3ebf63d40b0af9e13908
joaoedson <joaoedson.dantas@outlook.com>
2023-12-09 16:41:02 -0300

    feat: adding Setter to the Supply Entity properties

--- src/main/java/posto/abcd/api/entity/supply/SupplyEntity.java
+++ src/main/java/posto/abcd/api/entity/supply/SupplyEntity.java
@@ -2,10 +2,10 @@ package posto.abcd.api.entity.supply;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+import posto.abcd.api.dtos.supply.SupplyDataResquet;
+import posto.abcd.api.entity.fuelPump.FuelPumpEntity;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -21,12 +21,36 @@ public class SupplyEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     @Column(name = "date")
     private LocalDateTime date;
 
+    @Setter
+    @Min(0)
     private Long liters;
+
+    @Setter
+    @Min(0)
     private BigDecimal price;
+
+    @Setter
+    @Min(0)
     private int tax;
 
 
+    @Setter
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "fuel_pomp_id")
+    private FuelPumpEntity fuelPumpEntity;
+
+
+    public SupplyEntity(SupplyDataResquet supplyDataResquet, BigDecimal price, BigDecimal tax, FuelPumpEntity pump) {
+        if(supplyDataResquet.liters() > 0 ) {
+            this.setDate(supplyDataResquet.date());
+            this.setLiters(supplyDataResquet.liters());
+            this.setFuelPumpEntity(pump);
+            this.setTax(tax.byteValueExact());
+            this.setPrice(price);
+        }
+    }
 }
