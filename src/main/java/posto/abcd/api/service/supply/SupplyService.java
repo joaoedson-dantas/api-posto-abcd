3530f803c044057c1116030bc7987bd62108d060
joaoedson <joaoedson.dantas@outlook.com>
2023-12-10 16:06:35 -0300

    refactor: service supply fuel

--- src/main/java/posto/abcd/api/service/supply/SupplyService.java
+++ src/main/java/posto/abcd/api/service/supply/SupplyService.java
@@ -4,15 +4,13 @@ import jakarta.validation.ValidationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import posto.abcd.api.dtos.supply.SupplyDataResquet;
-import posto.abcd.api.entity.fuelPump.FuelPumpEntity;
 import posto.abcd.api.entity.supply.SupplyEntity;
 import posto.abcd.api.repository.fuelPump.FuelPumpRespository;
 import posto.abcd.api.repository.fuelTank.FuelTankRepository;
 import posto.abcd.api.repository.supply.SupplyRepository;
-import posto.abcd.api.service.fuelTank.FuelTankService;
 import posto.abcd.api.service.globalSettings.GlobalSettingsService;
-
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 public class SupplyService {
@@ -20,20 +18,17 @@ public class SupplyService {
     private final SupplyRepository supplyRepository;
     private final FuelPumpRespository fuelPumpRespository;
     private final FuelTankRepository fuelTankRepository;
-    private final FuelTankService fuelTankService;
     private final GlobalSettingsService globalSettingsService;
 
-    public SupplyService(SupplyRepository supplyRepository, FuelPumpRespository fuelPumpRespository, FuelTankRepository fuelTankRepository, FuelTankService fuelTankService, GlobalSettingsService globalSettingsService) {
+    public SupplyService(SupplyRepository supplyRepository, FuelPumpRespository fuelPumpRespository, FuelTankRepository fuelTankRepository, GlobalSettingsService globalSettingsService) {
         this.supplyRepository = supplyRepository;
         this.fuelPumpRespository = fuelPumpRespository;
         this.fuelTankRepository = fuelTankRepository;
-        this.fuelTankService = fuelTankService;
         this.globalSettingsService = globalSettingsService;
     }
 
     @Transactional
     public SupplyEntity supplyFuel(SupplyDataResquet supplyDataResquet) {
-
 
         if (!fuelPumpRespository.existsById(supplyDataResquet.fuel_pomp_id())) {
             throw new ValidationException("Id do bomba informado não existe!");
@@ -49,7 +44,6 @@ public class SupplyService {
             throw new ValidationException("A entidade FuelPump não existe no banco de dados.");
         }
 
-
         if (!fuelTankRepository.existsById(fuelPumpEntity.getFuelTankEntity().getId())) {
             throw new ValidationException("Id do tank informado não existe!");
         }
@@ -60,18 +54,18 @@ public class SupplyService {
             throw new IllegalArgumentException("A quantidade de combustível no tank é insuficiente para o abastecimento");
         }
 
+        var pricePerLiter = new BigDecimal(globalSettingsService.getGlobalSettingsByKey("fuel_price").value());
+        var milliliters = new BigDecimal(supplyDataResquet.liters());
+        var liters = milliliters.divide(BigDecimal.valueOf(1000));
+
+        var priceInAmount = pricePerLiter.multiply(liters);
+        var taxAmount = new BigDecimal(globalSettingsService.getGlobalSettingsByKey("tax_value").value());
+        var totalFuelPrice = priceInAmount.add(priceInAmount.multiply(taxAmount.divide(BigDecimal.valueOf(100))));
+
+        totalFuelPrice = totalFuelPrice.setScale(2, RoundingMode.HALF_UP);
+
+        var supplyEntity = new SupplyEntity(supplyDataResquet.date(), supplyDataResquet.liters(), totalFuelPrice, taxAmount.intValue(), fuelPumpEntity);
         tank.drain(supplyDataResquet.liters());
-
-        var price = (long) Integer.parseInt(globalSettingsService.getGlobalSettingsByKey("fuel_price").value());
-        var taxAmount = Integer.parseInt(globalSettingsService.getGlobalSettingsByKey("tax_value").value());
-        var priceDecimal = new BigDecimal(price);
-
-        var totalFuelPrice = priceDecimal.multiply(BigDecimal.ONE.add(BigDecimal.valueOf(taxAmount).divide(BigDecimal.valueOf(100))));
-
-
-        var supplyEntity = new SupplyEntity(supplyDataResquet.date(), supplyDataResquet.liters(), price, taxAmount, fuelPumpEntity);
         return supplyRepository.save(supplyEntity);
-
-
     }
 }
