ed956fc4c5c45c916a7367892d5bd2a2f33131e8
joaoedson <joaoedson.dantas@outlook.com>
2023-12-10 20:54:02 -0300

    feat: Added save encrypted password setting

--- src/main/java/posto/abcd/api/infra/security/SecurityConfigurations.java
+++ src/main/java/posto/abcd/api/infra/security/SecurityConfigurations.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // classe de configuracao
@@ -20,6 +22,11 @@ public class SecurityConfigurations {
                 })
         ;
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
