e33d753d665145d55f8202b945b717d1a7db6c8e
joaoedson <joaoedson.dantas@outlook.com>
2023-12-10 23:15:05 -0300

    feat: created controllet to authorization  with return token

---
+++ src/main/java/posto/abcd/api/controllers/user/AuthUserController.java
@@ -0,0 +1,29 @@
+package posto.abcd.api.controllers.user;
+
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import posto.abcd.api.dtos.user.AuthUserDTO;
+import posto.abcd.api.service.user.AuthUserService;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthUserController {
+
+    @Autowired
+    private AuthUserService authUserService;
+
+    @PostMapping("/user")
+    public ResponseEntity<Object> authenticate(@RequestBody @Valid AuthUserDTO authUserDTO) {
+        try {
+           var result = this.authUserService.execute(authUserDTO);
+           return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
