32f76240b11c143f4bd28d0ac10684238cb92f50
joaoedson <joaoedson.dantas@outlook.com>
2023-12-10 23:17:25 -0300

    refactor: validated with AuthUserDTO

--- src/main/java/posto/abcd/api/dtos/user/AuthUserDTO.java
+++ src/main/java/posto/abcd/api/dtos/user/AuthUserDTO.java
@@ -1,3 +1,11 @@
 package posto.abcd.api.dtos.user;
 
-public record AuthUserDTO (String password, String login) {}
+import jakarta.validation.constraints.NotBlank;
+
+public record AuthUserDTO (
+
+        @NotBlank
+        String password_hash,
+        @NotBlank
+        String login
+) {}
