855cc7c0e2d8b049319c1b86dfd70ba3cbf80f0c
joaoedson <joaoedson.dantas@outlook.com>
2023-12-12 10:55:50 -0300

    refactor: add check if there is a tank already registered with the same name

--- src/main/java/posto/abcd/api/services/fuelTank/CreateFuelTankService.java
+++ src/main/java/posto/abcd/api/services/fuelTank/CreateFuelTankService.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import posto.abcd.api.dtos.fuelTank.FuelTankDataRequest;
 import posto.abcd.api.entity.fuelTank.FuelTankEntity;
+import posto.abcd.api.infra.exceptions.TankAlreadyExistsException;
 import posto.abcd.api.repository.fuelTank.FuelTankRepository;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Service
 public class CreateFuelTankService {
@@ -15,8 +17,20 @@ public class CreateFuelTankService {
     @Autowired
     private FuelTankRepository repository;
 
+    @Autowired
+    private FindByFuelTypeService service;
+
     @Transactional
     public FuelTankEntity create(FuelTankDataRequest fuelTankDataRequest) {
+
+
+        var tankExists = service.findByFuelType(fuelTankDataRequest.name().toUpperCase());
+
+
+        if (Objects.equals(tankExists.getName(), fuelTankDataRequest.name().toUpperCase())) {
+            throw  new TankAlreadyExistsException(fuelTankDataRequest.name());
+        }
+
         var fuelTankEntity = new FuelTankEntity(fuelTankDataRequest, LocalDateTime.now());
         repository.save(fuelTankEntity);
         return fuelTankEntity;
