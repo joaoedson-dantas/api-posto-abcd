8e39838831f09d53b814018d5f6b2c7cfa06377a
joaoedson <joaoedson.dantas@outlook.com>
2023-12-11 23:36:40 -0300

    feat: Implementing method existsByLogin in UserRepository

--- src/main/java/posto/abcd/api/repository/user/UserRepository.java
+++ src/main/java/posto/abcd/api/repository/user/UserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity>findByLogin(String login);
+
+    boolean existsByLogin(String login);
 }
