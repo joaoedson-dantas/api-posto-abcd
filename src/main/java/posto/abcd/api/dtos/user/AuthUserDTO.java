40bead006fd06f6d561f2aaa59e06707332432a6
joaoedson <joaoedson.dantas@outlook.com>
2023-12-11 23:37:18 -0300

    refactor: DTO order reversed

--- src/main/java/posto/abcd/api/dtos/user/AuthUserDTO.java
+++ src/main/java/posto/abcd/api/dtos/user/AuthUserDTO.java
@@ -3,9 +3,10 @@ package posto.abcd.api.dtos.user;
 import jakarta.validation.constraints.NotBlank;
 
 public record AuthUserDTO (
+        @NotBlank
+        String login,
 
         @NotBlank
-        String password_hash,
-        @NotBlank
-        String login
+        String password_hash
+
 ) {}
