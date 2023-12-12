e1a95bd18f1d1f242655a1a82fee1bd22f517065
joaoedson <joaoedson.dantas@outlook.com>
2023-12-12 11:03:18 -0300

    refactor: renamed from FuelTankService to ListFuelTankService

--- src/main/java/posto/abcd/api/services/fuelTank/ListFuelTankService.java
+++ src/main/java/posto/abcd/api/services/fuelTank/ListFuelTankService.java
@@ -1,34 +1,25 @@
 package posto.abcd.api.services.fuelTank;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import posto.abcd.api.dtos.fuelTank.FuelTankDataList;
-import posto.abcd.api.dtos.fuelTank.FuelTankDataRequest;
-import posto.abcd.api.entity.fuelTank.FuelTankEntity;
 import posto.abcd.api.repository.fuelTank.FuelTankRepository;
 
-import java.time.LocalDateTime;
-
 @Service
-public class FuelTankService {
+public class ListFuelTankService {
 
 
     private final FuelTankRepository fuelTankRepository;
 
-    public FuelTankService(FuelTankRepository fuelTankRepository) {
+    public ListFuelTankService(FuelTankRepository fuelTankRepository) {
         this.fuelTankRepository = fuelTankRepository;
     }
 
 
-    @Transactional
     public Page<FuelTankDataList> list(Pageable pagination) {
         return fuelTankRepository.findAll(pagination)
                 .map(FuelTankDataList::new);
     }
-
-
 
 }
