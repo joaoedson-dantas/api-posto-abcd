da092458bf248ae327c4c74f15260d66e378a895
joaoedson <joaoedson.dantas@outlook.com>
2023-12-12 08:53:46 -0300

    feat: added the service responsible for updating global settings

---
+++ src/main/java/posto/abcd/api/service/globalSettings/UpdateGlobalSettingsService.java
@@ -0,0 +1,27 @@
+package posto.abcd.api.service.globalSettings;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import posto.abcd.api.entity.globalSettings.GlobalSettingsEntity;
+import posto.abcd.api.infra.exceptions.GlobalSettingsNotFoundException;
+import posto.abcd.api.repository.globalSettings.GlobalSettingsRepository;
+
+@Service
+public class UpdateGlobalSettingsService {
+
+    @Autowired
+    private GlobalSettingsRepository repository;
+
+    @Transactional
+    public GlobalSettingsEntity update(GlobalSettingsEntity globalSettings) {
+
+        if (!repository.existsById(globalSettings.getId())) {
+            throw new GlobalSettingsNotFoundException("Global Settings com ID " + globalSettings.getId() + " não encontrado.");
+        }
+
+        return repository.save(globalSettings);
+    }
+
+}
